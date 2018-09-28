ba8dd89fa66756b482160f4519120c136724a0f2
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 15:23:15 +0200

    Órai munka

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        /// <summary>
+        /// Szerző neve
+        /// </summary>
+        private string szerzo;
+        /// <summary>
+        /// Tartalom
+        /// </summary>
+        private string tartalom;
+        /// <summary>
+        /// Lájkok száma
+        /// </summary>
+        private int likeok = 0;
+        /// <summary>
+        /// Létrehozás dátuma
+        /// </summary>
+        private DateTime letrejott = DateTime.Now;
+        /// <summary>
+        /// Legutóbbi szerkesztés dátuma
+        /// </summary>
+        private DateTime szerkesztve = DateTime.Now;
+
+        public Bejegyzes()
+        {
+            szerzo = "Nincs";
+            tartalom = "-";
+            likeok = 0;
+            letrejott = DateTime.Now;
+            szerkesztve = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Új bejegyzés
+        /// </summary>
+        /// <param name="szerzo">A szerző neve</param>
+        /// <param name="tartalom">Tartalom</param>
+        public Bejegyzes(string szerzo, string tartalom)
+        {
+            this.szerzo = szerzo;
+            this.tartalom = tartalom;
+            this.likeok = 0;
+            this.letrejott = DateTime.Now;
+            this.szerkesztve = this.letrejott;
+        }
+
+       
+        /// <summary>
+        /// A bejegyzés minden adatát kiírja
+        /// </summary>
+        public void Kiir()
+        {
+            Console.WriteLine("Szerző: {0} - Lájkok: {1} - Létrejött: {2}", this.szerzo, this.likeok, this.letrejott);
+            if (this.letrejott != szerkesztve)
+            {
+                Console.WriteLine("Szerkesztve: {0}", this.szerkesztve);
+            }
+            Console.WriteLine(this.tartalom+"\n");
+
+        }
+
+        /*
+        public override string ToString()
+        {
+            return base.ToString();
+        }*/
+
+        public string GetSzerzo()
+        {
+            return this.szerzo;
+        }
+
+        public string GetTartalom()
+        {
+            return this.tartalom;
+        }
+
+        public void SetTartalom(string tartalom)
+        {
+            this.tartalom = tartalom;
+            this.szerkesztve = DateTime.Now;
+        }
+
+        public int GetLikeok()
+        {
+            return this.likeok;
+        }
+
+        public DateTime GetLetrejott()
+        {
+            return this.letrejott;
+        }
+
+        public DateTime GetSzerkesztve()
+        {
+            return this.szerkesztve;
+        }
+
+        public void Like()
+        {
+            this.likeok++;
+        }
+
+    }
+}
---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2a
+            List<Bejegyzes> lista = new List<Bejegyzes>();
+            List<Bejegyzes> lista2 = new List<Bejegyzes>();
+            //2b
+            Console.Write("Kérem adjon megy egy darabszámot: ");
+            int db = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < db; i++)
+            {
+                Console.Write("Kérem adjon meg egy bejegyzést\nSzerző: ");
+                string szerzo = Console.ReadLine();
+                Console.Write("Tartalom: ");
+                string tartalom = Console.ReadLine();
+                lista.Add(new Bejegyzes(szerzo, tartalom));
+            }
+            //2b+
+            StreamReader beolvas = new StreamReader("bejegyzesek.txt");
+            while (!beolvas.EndOfStream)
+            {
+                string sor = beolvas.ReadLine();
+                string[] adatok = sor.Split(';');
+
+                lista.Add(new Bejegyzes(adatok[0], adatok[1]));
+            }
+            beolvas.Close();
+
+            //2c
+            int bejegyzes20 = lista.Count() * 20;
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < bejegyzes20; i++)
+            {
+                int random = rnd.Next(0, lista.Count());
+                lista[random].Like();
+                //Console.WriteLine("A {0}. kapott 1 lájkot", random);
+            }
+
+            //2d
+            Console.Write("Kérem módosítja a 2. bejegyzést!\nTartalom: ");
+            lista[1].SetTartalom(Console.ReadLine());
+
+            //2e
+            //megnézni a listat
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                lista[i].Kiir();
+            }
+
+            //3a
+            for (int i = 0; i < lista.Count(); i++)
+            {
+
+            }
+
+        }
+    }
+}
