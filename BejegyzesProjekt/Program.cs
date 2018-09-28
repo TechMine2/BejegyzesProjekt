2eec17b4689f91eb3c53dd6008509c0e2273568f
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 15:54:04 +0200

    Összes feladat elkészítése

--- BejegyzesProjekt/Program.cs
+++ BejegyzesProjekt/Program.cs
@@ -13,7 +13,6 @@ namespace BejegyzesProjekt
         {
             //2a
             List<Bejegyzes> lista = new List<Bejegyzes>();
-            List<Bejegyzes> lista2 = new List<Bejegyzes>();
             //2b
             Console.Write("Kérem adjon megy egy darabszámot: ");
             int db = Convert.ToInt32(Console.ReadLine());
@@ -60,10 +59,69 @@ namespace BejegyzesProjekt
             }
 
             //3a
+            int legnepszerubbID = 0;
             for (int i = 0; i < lista.Count(); i++)
             {
-
+                if (legnepszerubbID < lista[i].GetLikeok())
+                {
+                    legnepszerubbID = i;
+                }
             }
+            Console.WriteLine("Legtöbb lájk: {0}", lista[legnepszerubbID].GetLikeok());
+
+            //3b
+            int seged = 0;
+            bool vanEOlyanBejegyzesAmi35NelTobb = false;
+            while (vanEOlyanBejegyzesAmi35NelTobb == false && seged < lista.Count())
+            {
+                if (lista[seged].GetLikeok() > 35)
+                {
+                    vanEOlyanBejegyzesAmi35NelTobb = true;
+                }
+                seged++;
+            }
+            if (vanEOlyanBejegyzesAmi35NelTobb)
+            {
+                Console.WriteLine("Van 35 lájknál több");
+            }
+
+            //3c
+            int hanyOlyanBejegyzesAmi15LajknalKevesebb = 0;
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                if (lista[i].GetLikeok() < 15)
+                {
+                    hanyOlyanBejegyzesAmi15LajknalKevesebb++;
+                }
+            }
+            Console.WriteLine("{0}db bejegyzés van aminek 15-nél kevesebb lájkja van.", hanyOlyanBejegyzesAmi15LajknalKevesebb);
+
+
+            Console.ReadKey();
+            //3d
+            for (int ID = lista.Count() - 1; ID > 0; ID--)
+            {
+                int maxID = ID;
+                for (int ID_2 = 0; ID_2 <= ID; ID_2++)
+                {
+                    if (lista[ID_2].GetLikeok() > lista[maxID].GetLikeok())
+                    {
+                        maxID = ID_2;
+                        Bejegyzes csere = lista[ID];
+                        lista[ID] = lista[maxID];
+                        lista[maxID] = csere;
+                    }
+                }
+            }
+            //megnézni a listat
+            for (int i = 0; i < lista.Count(); i++)
+            {
+                lista[i].Kiir();
+            }
+
+
+
+
 
         }
     }
